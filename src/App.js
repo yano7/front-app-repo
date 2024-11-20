251822616ba216e5597827916d0b84acac93c691
yano7 <yano77@naver.com>
2024-11-20 08:51:00 +0000

    version6

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version H5
+            EKS DEMO Blog version H6
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
