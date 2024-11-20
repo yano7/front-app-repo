844fb18c8c43bcc0c6dd4daec0eb63ef3b9a3799
yano7 <yano77@naver.com>
2024-11-20 06:37:03 +0000

    Add new blog version2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version H
+            EKS DEMO Blog version H2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
